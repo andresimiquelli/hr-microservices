538de85b9fcd833cffab8f069262f7c860b9c21c
João André Simiquelli <andresimiquelli@hotmail.com>
2022-05-29 14:34:51 -0300

    hr-payment mock implementation

---
+++ hr-payroll/src/main/java/com/andresimiquelli/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.andresimiquelli.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.andresimiquelli.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(long worker, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+}
