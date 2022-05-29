24694dfabbd16b09426ee27cd0b4a367879491fa
João André Simiquelli <andresimiquelli@hotmail.com>
2022-05-29 15:28:01 -0300

    Feign Client

--- hr-payroll/src/main/java/com/andresimiquelli/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/andresimiquelli/hrpayroll/services/PaymentService.java
@@ -1,13 +1,21 @@
 package com.andresimiquelli.hrpayroll.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andresimiquelli.hrpayroll.entities.Payment;
+import com.andresimiquelli.hrpayroll.entities.Worker;
+import com.andresimiquelli.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
+	
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
 
-	public Payment getPayment(long worker, int days) {
-		return new Payment("Bob", 200.0, days);
+	public Payment getPayment(long workerId, int days) {
+		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
